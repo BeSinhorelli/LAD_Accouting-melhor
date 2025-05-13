ad9e5ba4a3f97bc64c0e8b0dea39782d4d22d9dd
root <root@accounting.lad.pucrs.br>
2025-05-13 18:30:35 +0000

    update 'update.sh' to restart lad-dashboard.service

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Script de atualização automática do deploy da branch 'producao'
-# NÃO usar em ambiente de desenvolvimento
+cd /root/LAD_Accounting
 
-cd /root/LAD_Accounting || exit
-
-# Ativar o ambiente virtual
 source venv/bin/activate
 
 # Ir para a branch de produção
@@ -18,12 +14,7 @@ git fetch origin
 git merge origin/main --no-edit
 git merge origin/demandas --no-edit
 
-# Reiniciar o Gunicorn
-pkill gunicorn
-
-sleep 3
-
-cd source || exit
-gunicorn app:server --bind 0.0.0.0:8000 &
+# Reiniciar o serviço
+systemctl restart lad-dashboard.service
 
 echo "$(date): Atualização concluída" >> /root/LAD_Accounting/cron.log
